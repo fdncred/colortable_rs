86fead8907a28302eb10cd264b6f2a6874a00f6f
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2020-07-09 11:47:23 -0500

    updated table display

--- src/main.rs
+++ src/main.rs
@@ -96,22 +96,23 @@ fn main() {
 }
 
 pub fn print_table() {
-    let ansi_fg_sequences = vec!["m", "1m", "30m", "1;30m", "31m", "1;31m", "32m", "1;32m", "33m", "1;33m", "34m", "1;34m", "35m", "1;35m", "36m", "1;36m", "37m", "1;37m", ];
-    let ansi_bg_sequences = vec!["m", "40m", "41m", "42m", "43m", "44m", "45m", "46m", "47m", ];
+    let ansi_fg_sequences = vec!["30m", "1;30m", "31m", "1;31m", "32m", "1;32m", "33m", "1;33m", "34m", "1;34m", "35m", "1;35m", "36m", "1;36m", "37m", "1;37m", ];
+    let ansi_bg_sequences = vec!["40m", "41m", "42m", "43m", "44m", "45m", "46m", "47m", ];
 
     print!("\t");
     for (bg, bg_color) in ansi_bg_sequences.iter().enumerate() {
-        if bg > 0 { print!(" "); }
-        print!("  ");
-        if bg == 0 {
-            print!("   ");
-        } else {
-            // Write the column header
-            print!("{}", bg_color)
-        }
-        print!("  ");
+        // if bg > 0 { print!("1 "); }
+        // print!("2  ");
+        // if bg == 0 {
+        //     print!("3   ");
+        // } else {
+        //     // Write the column header
+        //     print!("{:>10}", bg_color)
+        // }
+        print!("{: >9}", bg_color);
+        print!(" ");
     }
-    print!("\n");
+    print!("\n"); 
 
     for (fg, fg_color) in ansi_fg_sequences.iter().enumerate() {
         let mut cur_fg:String;
@@ -120,7 +121,7 @@ pub fn print_table() {
         if fg >= 0 { 
             // Write the row header
             cur_fg = format!("{}\t", fg_color);
-            print!("{}{}", cur_fg, ANSI_RESET);
+            print!("{: >7}{}", cur_fg, ANSI_RESET);
         }
         if fg == 0 { 
             cur_fg = format!("{}{}", ANSI_PREFIX, "37m");
@@ -128,14 +129,15 @@ pub fn print_table() {
             cur_fg = format!("{}{}", ANSI_PREFIX, fg_color);
         }
 
-        for (bg, bg_item) in ansi_bg_sequences.iter().enumerate() {
+        for (bg, bg_color) in ansi_bg_sequences.iter().enumerate() {
             if bg > 0 { print!(" "); }
             if bg == 0 {
                 cur_bg = format!("{}{}", ANSI_PREFIX, "40m");
             } else {
-                cur_bg = format!("{}{}", ANSI_PREFIX, bg_item);
+                cur_bg = format!("{}{}", ANSI_PREFIX, bg_color);
             }
-            print!("{}{}{}{}", cur_fg, cur_bg, TEST_TEXT, ANSI_RESET);
+            // print!("{}{}{}{}{}", cur_fg, cur_bg, TEST_TEXT, ANSI_RESET);
+            print!("{: >7}{}{};{}{}", cur_fg, cur_bg, fg_color, bg_color, ANSI_RESET);
         }
         print!("\n");
     }
